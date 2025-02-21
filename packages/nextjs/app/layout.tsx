0e878906c2a8a8949eed880c5efc4aded0b83e01
Orlando Sanchez <pdigitalgroup@outlook.com>
2025-02-21 08:37:35 -0400

    3r commit

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -4,9 +4,9 @@ import "~~/styles/globals.css";
 import { ThemeProvider } from "~~/components/ThemeProvider";
 
 export const metadata: Metadata = {
-  title: "Scaffold-Stark",
-  description: "Fast track your starknet journey",
-  icons: "/logo.ico",
+  title: "Enhanced Altruism Protocol",
+  description: "A set of WEB3 tools that change the way donors and organizations interact. It increases transparency for organizations and gives donors more decision-making power, as well as rewards them for their actions.",
+  icons: "/favicon.ico",
 };
 
 const ScaffoldStarkApp = ({ children }: { children: React.ReactNode }) => {
